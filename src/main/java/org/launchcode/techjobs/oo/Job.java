c71203ecae5d0b35176495cb614b01c3b9423c8c
dprittj <dprittj@gmail.com>
2022-10-05 14:26:05 -0500

    GA2 Completed

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -17,9 +17,152 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String toString() {
+
+        if (name == ""){
+            setName("Data not available");
+        }
+        if (employer.getValue() == "") {
+            employer.setValue("Data not available");
+        }
+        if (location.getValue() == "") {
+            location.setValue("Data not available");
+        }
+        if (positionType.getValue() == "") {
+            positionType.setValue("Data not available");
+        }
+        if (coreCompetency.getValue() == "") {
+            coreCompetency.setValue("Data not available");
+        }
+
+//        checking/adding "Data not available" to blank name field
+//        do {
+//            getName();
+//            if (name == ""){
+//                setName("Data not available");
+//            }
+//        } while (name == "");
+
+//        checking/adding "Data not available" to null Employer field
+//        do {
+//            getEmployer();
+//            System.out.println("get: " + getEmployer());
+//            System.out.println("employer: " + employer);
+//            if (employer == null) {
+//                System.out.println("condition: true");
+//                setEmployer(new Employer("Data not available"));
+//                System.out.println("employer: " + employer);
+//            } else {
+//                System.out.println("condition: false");
+//            }
+//            System.out.println("employer: " + employer +"\n");
+//        } while (employer == null);
+
+//      checking/adding "Data not available" to blank Employer field
+//        do {
+//            employer.getValue();
+//            System.out.println("get:" + employer.getValue());
+//            System.out.println("employer: " + employer.getValue());
+//            if (employer.getValue() == "") {
+//                System.out.println("if condition: true");
+//                setEmployer(new Employer("Data not available"));
+//                System.out.println("employer: " + employer);
+//            } else {
+//                System.out.println("employer condition: false");
+//            }
+//            System.out.println("employer: " + employer + "\n");
+//        } while (employer.getValue() == "") ;
+
+        String jobString = "\nID: " + getId() +
+                        "\nName: " + getName() +
+                        "\nEmployer: " + getEmployer() +
+                        "\nLocation: " + getLocation() +
+                        "\nPosition Type: " + getPositionType() +
+                        "\nCore Competency: " + getCoreCompetency() +
+                        "\n";
+
+//        if (jobString.contains("\nEmployer: null")) {
+//            jobString.replaceFirst("\nEmployer: null", "Employer: Data not available");
+//        }
+        System.out.println(jobString);
+
+        return jobString;
+    }
+
+
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
 }
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,89 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId() {
+        Job aJob = new Job();
+        Job bJob = new Job();
 
+        assertNotEquals(aJob.getId(), bJob.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job newJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(newJob instanceof Job);
+        assertTrue(newJob.getEmployer() instanceof Employer);
+        assertTrue(newJob.getLocation() instanceof Location);
+        assertTrue(newJob.getPositionType() instanceof PositionType);
+        assertTrue(newJob.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals(newJob.getName(), "Product tester");
+        assertEquals(newJob.getEmployer().toString(), "ACME");
+        assertEquals(newJob.getLocation().toString(), "Desert");
+        assertEquals(newJob.getPositionType().toString(), "Quality control");
+        assertEquals(newJob.getCoreCompetency().toString(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job aJob = new Job("Product tester", new Employer ("ACME"), new Location ("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job bJob = new Job("Product tester", new Employer ("ACME"), new Location ("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(aJob.equals(bJob));
+        assertFalse(bJob.equals(aJob));
+
+    }
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Job newJob = new Job("Product tester", new Employer ("ACME"), new Location ("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        String jobObject = newJob.toString();
+
+        assertEquals(jobObject.charAt(0), '\n');
+        assertEquals(jobObject.charAt(jobObject.length()-1), '\n');
+    }
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData() {
+        Job newJob = new Job("Product tester", new Employer ("ACME"), new Location ("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals(newJob.toString(),
+                "\nID: " + newJob.getId() + "\nName: " + newJob.getName() + "\nEmployer: " + newJob.getEmployer() + "\nLocation: " + newJob.getLocation() + "\nPosition Type: " + newJob.getPositionType() + "\nCore Competency: " + newJob.getCoreCompetency() + "\n");
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField() {
+        Job newJob = new Job("", new Employer(""), new Location (""), new PositionType(""), new CoreCompetency(""));
+
+        System.out.println(newJob.toString());
+
+        assertTrue(newJob.toString().contains("Name: Data not available"));
+        assertTrue(newJob.toString().contains("Employer: Data not available"));
+        assertTrue(newJob.toString().contains("Location: Data not available"));
+        assertTrue(newJob.toString().contains("Position Type: Data not available"));
+        assertTrue(newJob.toString().contains("Core Competency: Data not available"));
+        assertEquals(newJob.toString(),
+                "\nID: " + newJob.getId() +
+                        "\nName: Data not available" +
+                        "\nEmployer: Data not available" +
+                        "\nLocation: Data not available" +
+                        "\nPosition Type: Data not available"  +
+                        "\nCore Competency: Data not available"  + "\n");
+
+    }
 }
